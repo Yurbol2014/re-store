b9add1d1c45d6db761d708773516ffc5c0c161f3
Bolotin Yuriy <simplovg@gmail.comgit config --global user.name Bolotin Yuriy>
2022-09-15 23:47:26 +0300

    Завершение стилей страницы магазина

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -1,31 +1,49 @@
-import React from 'react';
-import './shopping-cart-table.css';
+import React from "react";
+import "./shopping-cart-table.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMinusCircle,
+  faPlusCircle,
+  faTrashAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 const ShoppingCartTable = () => {
-
-
-
   return (
     <div className="shopping-cart-table">
       <h2>Your Order</h2>
       <table className="table">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Item</th>
-            <th>Count</th>
-            <th>Price</th>
-            <th>Action</th>
-          </tr>
-        </thead>
+        <tbody>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Item</th>
+              <th>Count</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
 
-     
+            <tr>
+              <td>1</td>
+              <td>Site Reliability Engineering</td>
+              <td>2</td>
+              <td>$40</td>
+              <td>
+              <button className="btn btn-outline-danger btn-sm float-right">
+                <FontAwesomeIcon icon={faTrashAlt} />
+              </button>
+              <button className="btn btn-outline-success btn-sm float-right">
+                <FontAwesomeIcon icon={faPlusCircle} />
+              </button>
+              <button className="btn btn-outline-warning btn-sm float-right">
+                <FontAwesomeIcon icon={faMinusCircle} />
+              </button>
+              </td>
+            </tr>
+          </thead>
+        </tbody>
       </table>
-
     </div>
   );
 };
-
-
 
 export default ShoppingCartTable;
